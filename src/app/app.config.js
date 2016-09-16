d1dd352eb7050ec4f6dd9a04ec57698bac90179d
Pieter-Jan Smets <Pieter-Jan.Smets@realdolmen.com>
2016-09-16 14:18:56 +0200

    add local search page

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -1,6 +1,12 @@
 export function appConfig($routeProvider) {
 
     $routeProvider.when("/movies/search", {
+        template: require("./movies-search-local/movies-search-local.html"),
+        controller: "MoviesSearchLocalController",
+        controllerAs: "ctrl"
+    });
+
+    $routeProvider.when("/movies/add", {
         template: require("./movies-search/movies-search.html"),
         controller: "MoviesSearchController",
         controllerAs: "ctrl"
--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,6 +4,7 @@ import { appConfig } from "./app.config";
 import { MyMoviesController } from "./my-movies/my-movies.controller";
 import { MoviesSearchController } from "./movies-search/movies-search.controller";
 import { MoviesDetailsController } from "./movies-details/movies-details.controller";
+import { MoviesSearchLocalController } from "./movies-search-local/movies-search-local.controller";
 import { MovieService } from "./shared/movie.service";
 // import { MoviesAddController } from "./contacts-add/contacts-add.controller";
 
@@ -16,6 +17,7 @@ export default angular.module("moviesApp", ["ngRoute"])
         .constant("apiUrl", "http://angularcoursebackend.azurewebsites.net/api")
         .controller("MyMoviesController", MyMoviesController)
         .controller("MoviesSearchController", MoviesSearchController)
+        .controller("MoviesSearchLocalController", MoviesSearchLocalController)
         .controller("MoviesDetailsController", MoviesDetailsController)
         .service("MovieService", MovieService)
         .component("moviesCollection", MoviesCollectionComponent)
