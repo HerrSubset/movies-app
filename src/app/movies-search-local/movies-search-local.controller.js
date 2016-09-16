0d962ef03db82befe9f74c0f477d35e1014e2206
Pieter-Jan Smets <Pieter-Jan.Smets@realdolmen.com>
2016-09-16 14:50:30 +0200

    local search page now links to movie details

--- src/app/movies-search-local/movies-search-local.controller.js
+++ src/app/movies-search-local/movies-search-local.controller.js
@@ -1,12 +1,19 @@
 export class MoviesSearchLocalController {
-    constructor(MovieService) {
+    constructor(MovieService, $location) {
+        this.$location = $location;
         this.MovieService = MovieService;
         this.movies = [];
+
+        this.actionText = "Details";
     }
 
     searchMovie(movieTitle) {
         this.MovieService.getCatalogMovies(movieTitle).then((result) => {
             this.movies = result;
         });
+    }
+
+    viewMovieDetails(selectedMovie) {
+        this.$location.path(`/movies/${selectedMovie.id}`)
     }
 }
