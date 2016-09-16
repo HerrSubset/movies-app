5e5d1c78b21899692ce5809dc93edd5ae795b0b1
Pieter-Jan Smets <Pieter-Jan.Smets@realdolmen.com>
2016-09-16 11:02:42 +0200

    use collection component to show movies in collection

--- src/app/my-movies/my-movies.controller.js
+++ src/app/my-movies/my-movies.controller.js
@@ -1,6 +1,9 @@
 export class MyMoviesController {
-    constructor() {
+    constructor(MovieService) {
         this.title = "My Movies";
+        MovieService.getAllCatalogMovies().then((movies) => {
+            this.movies = movies;
+        });
         this.init();
     }
 
--- src/app/shared/movie.service.js
+++ src/app/shared/movie.service.js
@@ -5,7 +5,13 @@ export class MovieService {
     }
 
     getOnlineMovies(title) {
-        return this.$http.get(`${this.apiUrl}/movies/online?title=${title}`).then((response) => {
+        return this.$http.get(`${this.apiUrl}/Movies/online?title=${title}`).then((response) => {
+            return response.data;
+        });
+    }
+
+    getAllCatalogMovies() {
+        return this.$http.get(`${this.apiUrl}/Movies`).then((response) => {
             return response.data;
         });
     }
